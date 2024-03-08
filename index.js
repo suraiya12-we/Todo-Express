86f7b756f1e57c745792f27ed5a18ae306544b32
Suraiya Midda <suraiyamiddasaj@gmail.com>
2024-03-08 15:40:50 +0530

    add

---
+++ index.js
@@ -0,0 +1,6 @@
+const express = require("express");
+const app = express();
+app.use(express.json());
+app.post("/todos", (req, res) => {});
+app.get("/todos", (req, res) => {});
+app.put("completed", (req, res) => {});
